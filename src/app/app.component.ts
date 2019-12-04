ab634789f4d595e1712fa8310de4982054eedb66
Sabrina Pomajulca <sabrina.pomajulca@tecsup.edu.pe>
2019-12-04 18:42:03 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductosService } from './productos.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularApp10';
+  lista=null;
+  prod = {
+  _id:null,
+  descripcion:null,
+  precio:null
+  }
+  constructor(private productosServicio: ProductosService) {}
+
+  ngOnInit() {
+    this.recuperarTodos();
+  }
+
+  recuperarTodos() {
+    this.productosServicio.listar().subscribe(result => {
+      this.lista = result;
+    });
+  }
+
+  nuevo() {
+    this.productosServicio.nuevo(this.prod).subscribe(result => {
+      if (result=='ok') {
+        this.limpiar();
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  eliminar(codigo) {
+  	if(!confirm("Esta seguro que desea eliminar este registro?"))
+  		return;
+    this.productosServicio.eliminar(codigo).subscribe(result => {
+      if (result=='ok') {
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  actualizar() {
+    this.productosServicio.actualizar(this.prod).subscribe(result => {
+      console.log(result)
+      if (result.nModified=='1') {
+        this.limpiar();
+        this.recuperarTodos();
+      }
+    });    
+  }
+  
+  mostrar(codigo) {
+    this.productosServicio.mostrar(codigo).subscribe(result => {
+      this.prod = result
+    });
+  }
+
+  hayRegistros() {
+    return true;
+  }
+  
+  limpiar(){
+    this.prod = { 
+      _id:null, 
+      descripcion:null, 
+      precio:null
+    };
+  }
+
 }
